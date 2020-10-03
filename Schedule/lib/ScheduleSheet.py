d06ce3b48551065ec92d7a7c73df153fb0c16637
Matt Schaller <matt.schaller@gmail.com>
2020-10-03 12:48:04 -0500

    refactored sheet code

--- Schedule/lib/ScheduleSheet.py
+++ Schedule/lib/ScheduleSheet.py
@@ -5,58 +5,24 @@ import os
 import os.path
 
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
+from Sheet import Sheet
 from ScheduleItem import ScheduleItem
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 SCHEDULE_PATH = os.path.join(DIR_PATH, 'schedule.pickle')
 
-class ScheduleSheet:
-    # If modifying these scopes, delete the file token.pickle.
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-    # The ID and range of a sample spreadsheet
-    SPREADSHEET_ID = '1WG7QDkulWYW8vlP0UvTqBXOGRchPr_uzETyjPKSDAL0'
-    RANGE_NAME = 'A2:G4'
-
-    CRED_PATH = os.path.join(DIR_PATH, 'credentials.json')
-    TOKEN_PATH = os.path.join(DIR_PATH, 'token.pickle')
-    SCHEDULE_PATH = os.path.join(DIR_PATH, 'schedule.pickle')
+class ScheduleSheet(Sheet):
 
     def __init__(self):
-        self._load_creds()
-           
-    def _load_creds(self):
-        creds = None
-        
-        # use a token.pickle file if it exists
-        if os.path.exists(ScheduleSheet.TOKEN_PATH):
-            with open(ScheduleSheet.TOKEN_PATH, 'rb') as token:
-                    creds = pickle.load(token)
-        
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(ScheduleSheet.CRED_PATH, ScheduleSheet.SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(ScheduleSheet.TOKEN_PATH, 'wb') as token:
-                pickle.dump(creds, token)
-        
-        self._creds = creds
+        super(ScheduleSheet, self).__init__('Schedule!A2:G4')
+
+    SCHEDULE_PATH = os.path.join(DIR_PATH, 'schedule.pickle')
 
     def _retrieve_schedule(self):
-        service = build('sheets', 'v4', credentials=self._creds)
-        sheet = service.spreadsheets() # pylint: disable=no-member
-        result = sheet.values().get(spreadsheetId=ScheduleSheet.SPREADSHEET_ID, range=ScheduleSheet.RANGE_NAME).execute()
-        values = result.get('values', [])
-
         items = []
 
-        for row in values:
+        for row in self._get_values():
             items.append(ScheduleItem(row)) 
 
         return items
---
+++ Schedule/lib/Sheet.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+import pickle
+import os
+import os.path
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from ScheduleItem import ScheduleItem
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+SCHEDULE_PATH = os.path.join(DIR_PATH, 'schedule.pickle')
+
+class Sheet(object):
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    # The ID and range of a sample spreadsheet
+    SPREADSHEET_ID = '1WG7QDkulWYW8vlP0UvTqBXOGRchPr_uzETyjPKSDAL0'
+    RANGE_NAME = None
+
+    CRED_PATH = os.path.join(DIR_PATH, 'credentials.json')
+    TOKEN_PATH = os.path.join(DIR_PATH, 'token.pickle')
+
+    def __init__(self, range_name):
+        self._load_creds()
+        self.RANGE_NAME = range_name
+           
+    def _load_creds(self):
+        creds = None
+        
+        # use a token.pickle file if it exists
+        if os.path.exists(Sheet.TOKEN_PATH):
+            with open(Sheet.TOKEN_PATH, 'rb') as token:
+                    creds = pickle.load(token)
+        
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(Sheet.CRED_PATH, Sheet.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(Sheet.TOKEN_PATH, 'wb') as token:
+                pickle.dump(creds, token)
+        
+        self._creds = creds
+    
+    def _get_values(self):
+        service = build('sheets', 'v4', credentials=self._creds)
+        sheet = service.spreadsheets() # pylint: disable=no-member
+        result = sheet.values().get(spreadsheetId=self.SPREADSHEET_ID, range=self.RANGE_NAME).execute()
+        return result.get('values', [])
